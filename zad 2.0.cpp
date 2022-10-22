84d9a83917fa485b629e66e391c974901e748a32
s27081 <115358253+s27081@users.noreply.github.com>
2022-10-22 14:53:51 +0200

    zad 2.0

---
+++ zad 2.0.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int kawalkiPizzy = 13;
+    int liczbaGosci = 5;
+
+    if (kawalkiPizzy % liczbaGosci == 0) {
+        cout << "Na kazdego uczestnika przypadaja " << kawalkiPizzy / liczbaGosci << " kawalki pizzy";
+    }
+    else {
+        int reszta = kawalkiPizzy % liczbaGosci;
+        
+        kawalkiPizzy - reszta;
+
+        cout << "Na kazdego uczestnika przypadaja " << (kawalkiPizzy - reszta) / liczbaGosci << " kawalki pizzy" << endl;;
+        cout << "Natomiast gospodarz otrzyma " << reszta;
+    }
+}
