07a1b9a961362f0719a3d9d8b9e55273d1c9a8f1
s27081 <115358253+s27081@users.noreply.github.com>
2022-10-22 16:55:10 +0200

    Add files via upload

---
+++ zad.2.2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+
+    cout << "Podaj wartosc a: ";
+    cin >> a;
+
+    cout << "Podaj wartosc a: ";
+    cin >> b;
+
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < b; j++) {
+            cout << "*";
+
+     }
+        cout << endl;
+   }
+    cout << "Obwod kwadratu: " << 2 * a + 2 * b << endl;
+}
